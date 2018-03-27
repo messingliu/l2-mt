ff5dd4c28fbaf9e06602c85fa42cdf837e960a4c
Meng Liu <mengliu@Mengs-MacBook-Air.local>
2018-03-26 21:57:13 -0700

    Add log properties for log print

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ http://localhost:8080/users?search=suggested,scenario-suggested&filter=&with=con
 return
 {"users":[{"id":1,"score":3.0,"popularity":22.0,"distance":1.0,"lastactivity":"none","type":"success"}]}
 
-
+Log data in ./l2-mt.log
+run ./gradlew bootRun --debug to show print out log
 
 # l2-mt
--- src/main/java/com/tantan/l2/clients/MergerClient.java
+++ src/main/java/com/tantan/l2/clients/MergerClient.java
@@ -2,12 +2,16 @@ package com.tantan.l2.clients;
 
 import com.tantan.l2.models.User;
 import com.tantan.l2.models.Users;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MergerClient {
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
     /**
      * This method will get a user from id
      * @param id - user id
@@ -16,7 +20,8 @@ public class MergerClient {
     public Users getUsers(Long id, String limit, String search, String filter, String with) {
         RestTemplate restTemplate = new RestTemplate();
         //TODO: change the url to merger url and replace the value to return
-        Users usersFromMerger = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Users.class);
+        String usersFromMerger = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", String.class);
+        LOGGER.info("usersFromMerger data is :  " + usersFromMerger.toString());
 
         User user = new User().setId(1L).setDistance(1).setLastactivity("none").setPopularity(22).setScore(3).setType("type");
         List<User> userList = new ArrayList<User>();
