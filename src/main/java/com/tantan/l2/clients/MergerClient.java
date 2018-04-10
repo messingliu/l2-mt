10e5688c3620f3cf4fac4b85260b4b65c2d4df4a
Meng Liu <mengliu@mengs-air.linkedin.biz>
2018-04-10 16:53:50 -0700

    Add a flag to disable merger call for local debug

--- src/main/java/com/tantan/l2/clients/MergerClient.java
+++ src/main/java/com/tantan/l2/clients/MergerClient.java
@@ -21,7 +21,7 @@ import java.util.List;
 @Component
 public class MergerClient {
   private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
-
+  private final boolean ableToCallMerger = true;
   /**
    * This method will get a user from id
    *
@@ -31,33 +31,35 @@ public class MergerClient {
   private final static String url_link = "http://127.0.0.1:8888/users?search=suggested,scenario-suggested" +
            "&filter=&with=contacts,questions,scenarios,user.publicMoments,relationships&user_id=";
   public Resp getUsers(Long id, int limit, String search, String filter, String with) {
-    RestTemplate restTemplate = new RestTemplate();
-    restTemplate.getMessageConverters().add(new JacksonConverter());
+    if (ableToCallMerger) {
+      //Get from merger
+      RestTemplate restTemplate = new RestTemplate();
+      restTemplate.getMessageConverters().add(new JacksonConverter());
+      String url = url_link + id + "&limit=" + limit;
+      //convert json to java object
+      ObjectMapper mapper = new ObjectMapper();
+      String usersFromMerger = restTemplate.getForObject(url, String.class);
+      Resp resp = null;
+      try {
+        resp = mapper.readValue(usersFromMerger, Resp.class);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
 
-    //Get from merger
-    String url = url_link + id + "&limit=" + limit;
-    //convert json to java object
-//       ObjectMapper mapper = new ObjectMapper();
-//       String usersFromMerger = restTemplate.getForObject(url, String.class);
-//       Resp userList = null;
-//       try {
-//           userList = mapper.readValue(usersFromMerger, Resp.class);
-//       } catch (Exception e) {
-//           e.printStackTrace();
-//       }
-//
-//       LOGGER.info("usersFromMerger data is :  " + usersFromMerger.toString());
-//       return userList;
+      LOGGER.info("usersFromMerger data is :  " + usersFromMerger.toString());
+      return resp;
+    } else {
 
-    //Test
-     User user1 = new User().setId(1L).setDistance(1).setLastactivity("none").setPopularity(22).setScore(3).setType("type");
-     User user2 = new User().setId(2L).setDistance(2).setLastactivity("none").setPopularity(33).setScore(4).setType("type");
-     User user3 = new User().setId(3L).setDistance(3).setLastactivity("none").setPopularity(44).setScore(5).setType("type");
+      //Test
+      User user1 = new User().setId(1L).setDistance(1).setLastactivity("none").setPopularity(22).setScore(3).setType("type");
+      User user2 = new User().setId(2L).setDistance(2).setLastactivity("none").setPopularity(33).setScore(4).setType("type");
+      User user3 = new User().setId(3L).setDistance(3).setLastactivity("none").setPopularity(44).setScore(5).setType("type");
 
-     List<User> userList = new ArrayList<User>();
-     userList.add(user1);
-     userList.add(user2);
-     userList.add(user3);
-     return new Resp().setMeta(new Meta(1L, "test")).setData(new UserList(userList)).setExtra(new Extra(false, 2));
+      List<User> userList = new ArrayList<User>();
+      userList.add(user1);
+      userList.add(user2);
+      userList.add(user3);
+      return new Resp().setMeta(new Meta(1L, "test")).setData(new UserList(userList)).setExtra(new Extra(false, 2));
+    }
   }
 }
