4389e762015c986227bbf3de8ac33300495632a4
pendesen <pendesen@163.com>
2018-04-20 13:47:44 +0800

    constants commit

---
+++ src/main/java/com/tantan/l2/constants/LogConstants.java
@@ -0,0 +1,13 @@
+package  ;
+
+public class LogConstants {
+    public static final String RESPONSE_TIME= "ResponseTime";
+    public static final String LOGO_TYPE= "LogType";
+    public static final String CLIENT_NAME= "ClientName";
+    public static final String DATA_SIZE= "DataSize";
+
+    public static final String CLIENT_CALL = "clientCall";
+    public static final String RANKER = "Ranker";
+    public static final String MERGER = "Merger";
+}
+
